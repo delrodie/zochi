35ccf9d40ae5093e389abd099979765277fe94cd
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2020-01-05 22:22:23 +0000

    Fixtures User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername("delrodie");
+        $user->setEmail("delrodieamoikon@gmail.com");
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'delroy@2020'));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
