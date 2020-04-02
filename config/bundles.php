d03f32f56ea13943d5e2554e9bedf6af1f14ebcf
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-04-02 04:00:38 +0000

    Paginator integrated

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Cocur\Slugify\Bridge\Symfony\CocurSlugifyBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ActiviteRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -11,10 +13,16 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(ActiviteRepository $activiteRepository)
+    public function index(Request $request, ActiviteRepository $activiteRepository, PaginatorInterface $paginator)
     {
+        $activiteListe = $activiteRepository->findListByDesc();
+
+        $activites = $paginator->paginate(
+            $activiteListe,
+            $request->query->getInt('page', 1),9
+        );
         return $this->render('default/index.html.twig', [
-            'activites' => $activiteRepository->findListByDesc(),
+            'activites' => $activites,
         ]);
     }
 }
